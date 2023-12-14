312f3ffa5455bad418889ed8eff6bac58eef42eb
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-12-14 15:23:51 +0530

    phase 1 front end complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
-import { BiHash, BiHomeCircle, BiUser } from "react-icons/bi";
+import { BiHash, BiHomeCircle, BiMoney, BiUser } from "react-icons/bi";
+import {SlOptions} from "react-icons/sl"
 import { Inter } from "next/font/google";
 import FeedCard from "@/components/FeedCard";
 const inter = Inter({ subsets: ["latin"] });
@@ -33,8 +34,16 @@ const sideBarMenuItems: TwitterSideBarButton[] = [
     icon: <BsBookmark />,
   },
   {
+    title: "Twitter Blue",
+    icon: <BiMoney />,
+  },
+  {
     title: "Profile",
     icon: <BiUser />,
+  },
+  {
+    title: "More Options",
+    icon: <SlOptions />,
   },
 ];
 
@@ -42,27 +51,32 @@ export default function Home() {
   return (
     <div className={inter.className}>
       <div className="grid grid-cols-12 h-screen px-56 w-screen">
-        <div className="  col-span-3 px-4  pt-8">
-          <div className="text-4xl h-fit w-fit hover:bg-gray-600 p-4  rounded-full cursor-pointer transition-all">
+        <div className="  col-span-3  ml-28 pt-1">
+          <div className="text-2xl h-fit w-fit hover:bg-gray-600 p-4  rounded-full cursor-pointer transition-all">
             <BsTwitter />
           </div>
-          <div className="mt-4 text-2xl  pr-4">
+          <div className="mt-1 text-1xl  pr-4">
             <ul>
               {sideBarMenuItems.map((item) => (
-                <li className="flex justify-start items-center gap-4 hover:bg-gray-600 rounded-full px-5 py-2 cursor-pointer w-fit mt-2">
-                  <span>{item.icon}</span>
-                  <span>{item.title}</span>
+                <li className="flex justify-start items-center gap-4 hover:bg-gray-600 rounded-full px-3 py-3 cursor-pointer w-fit mt-2">
+                  <span className="text-3xl">{item.icon}</span>
+                  <span className="text-xl">{item.title}</span>
                 </li>
               ))}
             </ul>
             <div className="mt-5 px-3">
-              <button className="bg-[#1B8FDB] font-semibold text-lg p-4 rounded-full w-full mt-5 mx-4">
+              <button className="bg-[#1B8FDB] font-semibold text-lg py-2  px-4 rounded-full w-full mt-5 ">
                 Tweet
               </button>
             </div>
           </div>
         </div>
-        <div className="col-span-6 border-r-[1px] border-l-[1px] border-gray-600">
+        <div className="col-span-5 border-r-[1px] border-l-[1px] border-gray-600 h-screen overflow-scroll">
+        <FeedCard/>
+        <FeedCard/>
+        <FeedCard/>
+        <FeedCard/>
+        <FeedCard/>
         <FeedCard/>
         </div>
         <div className="col-span-3">
--- components/FeedCard/index.tsx
+++ components/FeedCard/index.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import React from "react";
 import img from "../FeedCard/profile-photo.jpg"
+import { BiMessageRounded, BiUpload } from "react-icons/bi";
+import {FaRetweet} from "react-icons/fa"
+import {AiOutlineHeart} from "react-icons/ai"
 
 const FeedCard: React.FC = () => {
     
   return (
-    <div className="border border-gray-600 p-4 hover:bg-slate-800 transition-all cursor-pointer">
-      <div className="grid grid-cols-12">
+    <div className="border border-r-0 border-l-0 border-b-0 p-5 border-gray-600 p-4 hover:bg-slate-800 transition-all cursor-pointer">
+      <div className="grid grid-cols-12 gap-3">
         <div className="col-span-1">
           <Image className="rounded-full"
             src={img}
@@ -24,6 +27,23 @@ const FeedCard: React.FC = () => {
 
             
             </p>
+            <div className="flex justify-between mt-5 text-xl items-center p-2 w-[90%]">
+                <div>
+                    <BiMessageRounded/>
+                </div>
+                <div>
+                    <FaRetweet/>
+                </div>
+                <div>
+                    <AiOutlineHeart/>
+                </div>
+                <div>
+                    <BiUpload/>
+                </div>
+                <div>
+                    <BiMessageRounded/>
+                </div>
+            </div>
           
         </div>
       </div>
